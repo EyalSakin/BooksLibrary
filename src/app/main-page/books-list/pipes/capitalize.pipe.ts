d2ac1ff6a71d437a34db90b1a64587a604ce187d
Eyal Sakin <eyal@Eyals-MacBook-Pro.local>
2017-10-02 15:06:23 +0300

    Changed Pipe

--- src/app/main-page/books-list/pipes/capitalize.pipe.ts
+++ src/app/main-page/books-list/pipes/capitalize.pipe.ts
@@ -3,11 +3,21 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({name: 'capitalize'})
 export class CapitalizePipe implements PipeTransform {
 
-    transform(value:any) {
-        if (value) {
-            return value.replace(/\b\w/g, first => first.toLocaleUpperCase());
-        }else{
-            return value.charAt(0).toUpperCase() + value.slice(1);
-        }
+    // transform(value:any) {
+    //     if (value) {
+    //         return value.replace(/\b\w/g, first => first.toLocaleUpperCase());
+    //     }else{
+    //         return value.charAt(0).toUpperCase() + value.slice(1);
+    //     }
+    // }
+
+    transform(value:string) {
+      var validString: string = value.replace(/[^a-zA-Z]/g, ' ');
+
+      if (validString) {
+        return validString.replace(/\b\w/g, first => first.toLocaleUpperCase());
+      }else{
+        return validString.charAt(0).toUpperCase() + value.slice(1);
+      }
     }
 }
